3db7033cec740288559e2747a5b2254c904a78fb
Juan Bogado <bogadojuanmiguel@gmail.com>
2023-07-08 14:17:02 -0300

    arreglo de botones

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -76,6 +76,7 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: "row",
         alignItems: "center",
+        gap: 5
     },
     button: {
         borderRadius: 20,
